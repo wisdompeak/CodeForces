ed41fc6a9e7ad1573fa7c3ec3b211315c2a18285
wisdompeak <guan.huifeng@gmail.com>
2019-05-20 13:14:26 -0700

    Update C.Match-Points.cpp

--- Edu_Round_64/C.Match-Points/C.Match-Points.cpp
+++ Edu_Round_64/C.Match-Points/C.Match-Points.cpp
@@ -8,15 +8,15 @@ void solution(int n, int z, vector<int>&nums)
     sort(nums.begin(),nums.end());
 
     int i;
-    int j=0;
-    for (i=0; i<n; i++)
+    int j=n/2;
+    for (i=0; i<n/2; i++)
     {
         while (j<n && nums[j]-nums[i]<z)
             j++;
         if (j==n) break;
         j++;
     }
-    cout<<min(n/2,i)<<endl;
+    cout<<i<<endl;
 }
 
 int main() {
