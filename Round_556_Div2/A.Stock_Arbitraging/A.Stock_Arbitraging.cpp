2553068ec55772f29fe39e03387a9da60893d24c
wisdompeak <guan.huifeng@gmail.com>
2019-05-19 22:09:35 -0700

    Update A.Stock_Arbitraging.cpp

--- Round_556_Div2/A.Stock_Arbitraging/A.Stock_Arbitraging.cpp
+++ Round_556_Div2/A.Stock_Arbitraging/A.Stock_Arbitraging.cpp
@@ -1,4 +1,3 @@
-// #include <QCoreApplication>
 #include <fstream>
 #include <iostream>
 #include <string.h>
@@ -8,32 +7,6 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-
-
-int solution(int n, int m, int r, vector<int>&buy, vector<int>&sell)
-{
-    int Sell = *max_element(sell.begin(),sell.end());
-    vector<int>Buy;
-    for (int x:buy)
-        if (x<Sell) Buy.push_back(x);
-
-    vector<int>dp(r+1,0);
-    for (int vol=1; vol<=r; vol++)
-    {
-        auto dp_temp = dp;
-        for (int i=0; i<Buy.size(); i++)
-        {
-            if (vol-Buy[i]<0) continue;
-            dp[vol] = max(dp[vol], dp_temp[vol-Buy[i]]+Sell-Buy[i]);
-        }
-    }
-
-    int result = 0;
-    for (int v=1; v<=r; v++)
-        result = max(result, dp[v]+r);
-    return result;
-
-}
 
 
 int main()
@@ -50,7 +23,14 @@ int main()
     for (int i=0; i<m; i++)
         cin>>sell[i];
 
-    cout<<solution(n,m,r,buy,sell)<<endl;
-
+    sort(buy.begin(),buy.end());
+    sort(sell.begin(),sell.end());
+    
+    if (sell[m-1]<=buy[0])
+        cout<<r<<endl;
+    else
+    {
+        int k = r/buy[0];
+        cout<<r-k*buy[0]+k*sell[m-1]<<endl;
+    }    
 }
-
